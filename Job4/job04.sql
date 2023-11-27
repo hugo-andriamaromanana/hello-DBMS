b29020a10fc96a536053f9259985d8466cca4ac5
hugo-andriamaromanana <hugo-andriamaromanana>
2023-11-27 16:28:50 +0100

    [feat] job3

---
+++ Job4/job04.sql
@@ -0,0 +1,29 @@
+--1
+SELECT
+    *
+FROM
+    students;
+
+--2
+SELECT
+    *
+FROM
+    students
+WHERE
+    age > 20;
+
+--3
+SELECT
+    *
+FROM
+    students
+ORDER BY
+    grade ASC;
+
+--4
+SELECT
+    *
+FROM
+    students
+ORDER BY
+    grade DESC;
